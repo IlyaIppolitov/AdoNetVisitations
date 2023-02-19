8fe0b663ab9c1e0e5f3485869a598e8ba6e9c1fb
FinT08 <IppolitovIS@gmail.com>
2023-02-19 17:15:21 +0300

    Добавьте файлы проекта.

---
+++ AdoNetVisitations/Program.cs
@@ -0,0 +1,116 @@
+﻿using AdoNetVisitations;
+using System.Dynamic;
+
+var dbService = new StudentsVisitationService();
+
+dbService.CreateTableStudents();
+dbService.CreateTableVisitations();
+dbService.FillTestStudents();
+dbService.FillTestVisitations();
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Введите команду:\n" +
+        "\"Students\" - для получения списка посещений;\n" +
+        "\"AddVisit\" - для новой записи в список посещений;\n" +
+        "\"AddStudent\" - для новой записи в список студентов;\n" +
+        "Для выхода введите \"0\"."); ;
+    var command = Console.ReadLine();
+
+    if (command != null) command = command.ToLower();
+    else
+    {
+        Console.WriteLine("Попробуйте ещё раз!");
+        Console.ReadKey();
+    }
+
+    if (command == "0") break;
+
+    string name = string.Empty;
+    string surname = string.Empty;
+
+    switch (command)
+    {
+        case "students":
+
+            Visitation[] Visitations = dbService.GetVisitations();
+            if (Visitations.Count() == 0 )
+            {
+                Console.WriteLine("Нет информации о посещениях!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Список посещений:");
+                foreach (var it in Visitations)
+                {
+                    Console.WriteLine(it.ToString());
+                }
+                Console.ReadKey();
+            }
+            break;
+
+        case "addvisit":
+            Console.WriteLine("Введите имя:");
+            while (true)
+            {
+                name = Console.ReadLine();
+                if (name == null) Console.WriteLine("Поле не может быть пустым! \nПопробуйте ещё раз!");
+                else break;
+            }
+            Console.WriteLine("Введите фамилию:");
+            while (true)
+            {
+                surname = Console.ReadLine();
+                if (surname == null) Console.WriteLine("Поле не может быть пустым! \nПопробуйте ещё раз!");
+                else break;
+            }
+
+            if (!dbService.CheckStudentByName(name, surname))
+            {
+                Console.WriteLine("Нет студента с таким имененм в базе!");
+                Console.ReadKey();
+                break;
+            }
+
+            DateOnly date;
+            while (true)
+            {
+                Console.WriteLine("Введите дату в формате dd.MM.yyyy:");
+                if (DateOnly.TryParseExact(Console.ReadLine(), "dd.MM.yyyy", out date))
+                {
+                    break;
+                }
+                else Console.WriteLine("Соблюдайте формат!");
+
+            }
+            dbService.AddVisitation(name,surname,date);
+            Console.WriteLine("DONE!");
+            Console.ReadKey();
+            break;
+
+        case "addstudent":
+            Console.WriteLine("Введите имя:");
+            while (true)
+            {
+                name = Console.ReadLine();
+                if (name == null) Console.WriteLine("Поле не может быть пустым! \nПопробуйте ещё раз!");
+                else break;
+            }
+            Console.WriteLine("Введите фамилию:");
+            while (true)
+            {
+                surname = Console.ReadLine();
+                if (surname == null) Console.WriteLine("Поле не может быть пустым! \nПопробуйте ещё раз!");
+                else break;
+            }
+            
+            dbService.AddStudent(name,surname);
+            Console.WriteLine("DONE!");
+            Console.ReadKey();
+            break;
+
+            default: break;
+    }
+}
---
+++ AdoNetVisitations/StudentsVisitationService.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AdoNetVisitations
+{
+    internal class StudentsVisitationService
+    {   
+        public Visitation[] GetVisitations()
+        {
+            var connectionString = "Data Source=myapp.db";
+            var sql = "select * from Visitations";
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+            using var command = new SqliteCommand(sql, connection);
+            using var reader = command.ExecuteReader();
+            var result = new List<Visitation>();
+            foreach (IDataRecord row in reader)
+            {
+                var visitation = new Visitation(
+                    row.GetInt64(row.GetOrdinal("Id")),
+                    row.GetString(row.GetOrdinal("Name")),
+                    row.GetString(row.GetOrdinal("Surname")),
+                    DateOnly.FromDateTime(DateTime.Parse(row.GetString(row.GetOrdinal("Date"))))
+                );
+                result.Add(visitation);
+            }
+            return result.ToArray();
+        }
+        public Student[] GetStudents()
+        {
+            var connectionString = "Data Source=myapp.db";
+            var sql = "select * from Students";
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+            using var command = new SqliteCommand(sql, connection);
+            using var reader = command.ExecuteReader();
+            var result = new List<Student>();
+            foreach (IDataRecord row in reader)
+            {
+                var student = new Student(
+                    row.GetInt64(row.GetOrdinal("Id")),
+                    row.GetString(row.GetOrdinal("Name")),
+                    row.GetString(row.GetOrdinal("Surname"))
+                );
+                result.Add(student);
+            }
+            return result.ToArray();
+        }
+
+        public bool CheckStudentByName(string name, string surname)
+        {
+            var connectionString = "Data Source=myapp.db";
+            var sql = $"select * from Students where Name like '{name}' and Surname like '{surname}'";
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+            using var command = new SqliteCommand(sql, connection);
+            using var reader = command.ExecuteReader();
+            var result = new List<Student>();
+            foreach (IDataRecord row in reader)
+            {
+                var student = new Student(
+                    row.GetInt64(row.GetOrdinal("Id")),
+                    row.GetString(row.GetOrdinal("Name")),
+                    row.GetString(row.GetOrdinal("Surname"))
+                );
+                result.Add(student);
+            }
+            
+            return result.Count > 0;
+        }
+
+        // создание таблицы с учётом проверки её сущестования
+        public void CreateTableVisitations()
+        {
+            var connectionString = "Data Source = myapp.db";
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+            using var command = connection.CreateCommand();
+            command.CommandText = $"create table if not exists Visitations " +
+                $"(id integer not null primary key, " +
+                $"Name text not null," +
+                $"Surname text not null," +
+                $"Date date not null )";
+            command.ExecuteNonQuery();
+        }
+        public void CreateTableStudents()
+        {
+            var connectionString = "Data Source = myapp.db";
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+            using var command = connection.CreateCommand();
+            command.CommandText = $"create table if not exists Students " +
+                $"(id integer not null primary key, " +
+                $"Name text not null," +
+                $"Surname text not null )";
+            command.ExecuteNonQuery();
+        }
+
+        public void AddVisitation(string name, string surname, DateOnly date)
+        {
+            var connectionString = "Data Source = myapp.db";
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+            using var command = connection.CreateCommand();
+            command.CommandText = $"insert into Visitations (Name, Surname, Date) values" +
+                $"('{name}','{surname}','{date}')";
+            command.ExecuteNonQuery();
+        }
+
+        public void AddStudent(string name, string surname)
+        {
+            var connectionString = "Data Source = myapp.db";
+            using var connection = new SqliteConnection(connectionString);
+            connection.Open();
+            using var command = connection.CreateCommand();
+            command.CommandText = $"insert into Students (Name,Surname) values" +
+                $"('{name}','{surname}')";
+            command.ExecuteNonQuery();
+        }
+
+        public void FillTestVisitations()
+        {
+            for (int i = 0 ; i < 10; i++)
+            {
+                AddVisitation("Name" + i+1, "Surname" + i+1, new DateOnly(2022, 1, i+1));
+            }
+        }
+
+        public void FillTestStudents()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                AddStudent("Name" + i+1, "Surname" + i+1);
+            }
+        }
+    }
+
+    internal class Visitation
+    {
+        public Visitation(long id, string name, string surname, DateOnly dateOnly) {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Date = dateOnly;
+        }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateOnly Date { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id.ToString()} - {Name} - {Surname} - {Date.ToString()}";
+        }
+    }
+
+    internal class Student
+    {
+        public Student(long id, string name, string surname)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id.ToString()} - {Name} - {Surname}";
+        }
+    }
+}
